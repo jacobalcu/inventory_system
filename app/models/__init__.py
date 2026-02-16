9867fd04420b727110f083f3be3db271aa8fdb6c
jalcumb <101678531+jacobalcu@users.noreply.github.com>
2026-02-16 13:01:54 -0500

    feat(models): Created Models for Users, Orders, and Products

---
+++ app/models/__init__.py
@@ -0,0 +1,6 @@
+from .user import User  # Allows us to import User easily elsewhere
+from .product import Product  # Allows us to import Product easily elsewhere
+from .order import (
+    Order,
+    OrderItem,
+)  # Allows us to import Order and OrderItem easily elsewhere
